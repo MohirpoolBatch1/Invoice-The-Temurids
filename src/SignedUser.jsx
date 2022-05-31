6ad0f13fdd69ecfee66479e9e7a0c2ff1fd88848
umarmuhandis <umarmuhandis@gmail.com>
2022-05-31 16:53:02 +0500

    add extensions

--- src/SignedUser.jsx
+++ src/SignedUser.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+import React from 'react'
 
-const SignedUser = () => {
-  return (
-    <div className="App font-spartan font-medium h-screen w-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-purple">Kod yozishni boshlaymiz. Barchaga omad 🚀</h1>
-    </div>
-  );
-};
-export default SignedUser;
+const SignedUser = () => (
+  <div className="App font-spartan font-medium h-screen w-screen flex items-center justify-center bg-gray-100">
+    <h1 className="text-purple">Kod yozishni boshlaymiz. Barchaga omad 🚀</h1>
+  </div>
+)
+export default SignedUser
