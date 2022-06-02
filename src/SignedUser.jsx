676cf33fb663dd022a6b49c20ba1ab894b56fae6
Temurbek <valievtemurbek@gmail.com>
2022-06-02 21:38:20 +0500

    Sidebar updated

--- src/SignedUser.jsx
+++ src/SignedUser.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Sidebar from "./components/Sidebar.jsx";
+/* import Homepage from "./pages/Homepage/Homepage.jsx"; */
 
-const SignedUser = () => {
-  return (
-    <div className="App font-spartan font-medium h-screen w-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-purple">Kod yozishni boshlaymiz. Barchaga omad 🚀</h1>
-    </div>
-  );
-};
+const SignedUser = () => (
+  <div className="App font-spartan font-medium h-screen w-screen flex bg-gray-100">
+    <Sidebar />
+    {/*     <Routes>   <Route path="/" element={<Homepage />} /> </Routes> */}
+  </div>
+);
 export default SignedUser;
