656807c55c1f070d2dc9ed0a00c4d727831af58e
ibrohimsoft <ahatkulov98@gmail.com>
2022-06-02 17:23:32 +0500

    IT-2 main branch clear

--- src/SignedUser.jsx
+++ src/SignedUser.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-import Button from "./components/Button.jsx";
+import React from 'react'
 
 const SignedUser = () => (
   <div className="App font-spartan font-medium h-screen w-screen flex items-center justify-center bg-gray-100">
     <Button type="editLight" />
   </div>
 )
-export default SignedUser;
+export default SignedUser
