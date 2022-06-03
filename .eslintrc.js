9ea485383ebf942bf7832190d00c6d9814170dc7
Temurbek <valievtemurbek@gmail.com>
2022-06-03 22:45:43 +0500

    IT-3 Sidebar

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,29 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  extends: ['airbnb-base', 'prettier'],
   parser: '@babel/eslint-parser',
-  extends: ['airbnb-base', 'prettier', 'eslint:recommended', 'react-app'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+    requireConfigFile: false,
   },
-  plugins: ['prettier'],
+
+  plugins: ['react', 'import', 'jsx-a11y', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-unused-vars': 'off',
     'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
     'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
-    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}],
+    'default-param-last': 'off',
   },
 }
