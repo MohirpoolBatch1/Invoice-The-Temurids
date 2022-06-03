6a88af277200ccc11c4ee62b0ccb0a3be94501fe
Temurbek <valievtemurbek@gmail.com>
2022-06-03 23:55:08 +0500

    IT-3 Sidebar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App";
-import { store } from "./app/store";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import App from './App'
+import {store} from './app/store'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById("main"));
+const root = ReactDOM.createRoot(document.getElementById('main'))
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
