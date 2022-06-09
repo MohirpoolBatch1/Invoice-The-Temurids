6b187749c833993446151c00a36f42a707adf3e6
ibrohimsoft <ahatkulov98@gmail.com>
2022-06-09 21:41:26 +0500

    IT-11 View invoice romponent ready

--- src/SignedUser.jsx
+++ src/SignedUser.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import InvoiceItemPage from './pages/InvoiceItemPage/InvoiceItemPage.jsx'
 import Sidebar from './components/Sidebar.jsx'
-/* import Homepage from "./pages/Homepage/Homepage.jsx"; */
+import Homepage from './pages/Homepage/Homepage.jsx'
+import datas from './data.json'
 
 const SignedUser = () => (
   <div className="App font-spartan font-medium h-screen w-screen flex bg-gray-100">
     <Sidebar />
-    {/*     <Routes>   <Route path="/" element={<Homepage />} /> </Routes> */}
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      {datas.map((data, index) => (
+        <Route
+          key={index}
+          path={`/invoice/${data.id}`}
+          element={<InvoiceItemPage id={data.id} />}
+        />
+      ))}
+    </Routes>
   </div>
 )
 export default SignedUser
