600030b6ec18d3d7019558480b98fd9a316de274
otabek <fozilovotabek424@gmail.com>
2022-07-13 16:41:17 +0500

    fix(IT-9)-filter-by-status-homepage-rebased

--- src/app/store.js
+++ src/app/store.js
@@ -1,23 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
-
-// TODO: Default reducer yozilgan, bunga hozircha tegmay turing.
-const initialState = {value: 0}
-
-function counterReducer(state = initialState, action = {}) {
-  switch (action.type) {
-    case 'increment':
-      return {...state, value: state.value + 1}
-    case 'decrement':
-      return {...state, value: state.value - 1}
-    case 'incrementByAmount':
-      return {...state, value: state.value + action.payload}
-    default:
-      return state
-  }
-}
+import {invoiceApi} from '../services/invoiceApi'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [invoiceApi.reducerPath]: invoiceApi.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(invoiceApi.middleware),
 })
