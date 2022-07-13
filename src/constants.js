ced4f3297379092ea5c677216729f3b26a01bb5d
otabek <fozilovotabek424@gmail.com>
2022-07-13 16:44:11 +0500

    fix(IT-9)-storeJs-modified

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export const bgColor = '#F2F2F2'
-=======
 const bgColor = '#F2F2F2'
 const baseUrl = 'https://serene-plains-38345.herokuapp.com/'
 export {baseUrl, bgColor}
->>>>>>> 6d91b1a (feat(IT-9)-filter-by-status-isFinished)
