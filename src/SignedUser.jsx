2a9c6605809f644ab19c1bf96a909c08ca9ef41e
otabek <fozilovotabek424@gmail.com>
2022-07-13 17:14:48 +0500

    feat(IT-9)-filter-by-status-homepage-modified

--- src/SignedUser.jsx
+++ src/SignedUser.jsx
@@ -8,11 +8,7 @@ import {bgColor} from './constants'
 
 const SignedUser = () => (
   <div
-<<<<<<< HEAD
     className={`App font-spartan font-medium h-screen w-screen flex  bg-[${bgColor}]`}
-=======
-    className={`App font-spartan font-medium h-screen w-screen flex bg-[#F2F2F2] bg-[${bgColor}]`}
->>>>>>> 6d91b1a (feat(IT-9)-filter-by-status-isFinished)
   >
     <InvoiceRoutes />
   </div>
