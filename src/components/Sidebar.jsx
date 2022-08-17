f5d4c9898c362d50840ba99e005caf7db5e6e5b2
otabek <fozilovotabek424@gmail.com>
2022-08-17 14:43:25 +0500

    fix(IT-31)-theme-changing-modified

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'
+import React, {useContext} from 'react'
 import {Link} from 'react-router-dom'
 import avatar from '../assets/image-avatar.jpg'
 import logo from '../assets/logo.svg'
