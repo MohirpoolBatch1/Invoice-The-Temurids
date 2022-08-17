af97a13c79d70b1687f529ba992a0997f8e495a9
Temurbek <valievtemurbek@gmail.com>
2022-08-17 10:45:22 +0500

    fix(IT-32)

--- src/components/ErrorBoundaries/ErrorFallback.jsx
+++ src/components/ErrorBoundaries/ErrorFallback.jsx
@@ -1,9 +1,15 @@
-export default function ErrorFallback({error, resetErrorBoundary}) {
+export default function ErrorFallback({error, resetErrorBoundary, children}) {
   return (
     <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
+      {error ? (
+        <div>
+          <p>Something went wrong:</p>
+          <pre>{error.message}</pre>
+          <button onClick={resetErrorBoundary}>Try again</button>
+        </div>
+      ) : (
+        children
+      )}
     </div>
   )
 }
